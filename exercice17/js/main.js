b5996792fb1d0d8bc9f20a27f145f760689c0b20
Nicko <nickos@iMac.local>
2022-01-17 16:07:19 +0100

    exo18-end

--- exercice17/js/main.js
+++ exercice17/js/main.js
@@ -5,7 +5,9 @@ $(() => {
         const text = $("#text");
         text.show();
 
-        //la methode .show pourrait correspondre a un display: unset;
+        //la methode .show pourrait correspondre a un display: unset (même si cependant il n'existe pas encore d'opposé a display: none) 
+        //ou à visibility: visible.
+
 
     });
 
---
+++ exercice18/js/main.js
@@ -0,0 +1,13 @@
+$(() => {
+
+    $(".btn").click(function() {
+
+        const text = $("#text");
+        text.hide();
+
+        //la methode .show pourrait correspondre a un display: none, ou à un visibility: visible.
+
+
+    });
+
+});
