a28a3abfa5e5006c15b586e8f4270b84928ced6e
Nicko <nickos@iMac.local>
2022-01-17 16:24:17 +0100

    exo19-end

---
+++ exercice19/js/main.js
@@ -0,0 +1,12 @@
+$(() => {
+
+    $(".btn").click(function() {
+
+        const area = $("input");
+        area.removeAttr("disabled");
+
+        // .removeAttr() enlève un attribut
+
+    });
+
+});
