f8776c9b4a6824ba5b5956ffe92eaa0f84f3f3d8
Nicko <nickos@iMac.local>
2022-01-17 16:02:14 +0100

    exo17-end

---
+++ exercice17/js/main.js
@@ -0,0 +1,12 @@
+$(() => {
+
+    $(".btn").click(function() {
+
+        const text = $("#text");
+        text.show();
+
+        //la methode .show pourrait correspondre a un display: unset;
+
+    });
+
+});
