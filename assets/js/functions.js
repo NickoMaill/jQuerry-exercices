5b749985d47e3e00c16652791597cc2d6c9568d3
Nicko <nickos@iMac.local>
2022-01-17 15:32:26 +0100

    exo9-end

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,12 @@
 function displaySecretText() {
     $('#exercise #text').show();
 }
+
+//Ma fonction d'affiche
+
+function showMyText() {
+
+    const text = $("#text");
+    text.css("display", "flex");
+
+}
