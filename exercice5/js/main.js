b54f2bcc5f1e8ecd86e3acc03150bce353dc9999
Nicolas-Maillols <nicomaillols@gmail.com>
2022-01-18 16:06:25 +0100

    exo5-vanilla-end

--- exercice5/js/main.js
+++ exercice5/js/main.js
@@ -1,6 +1,10 @@
-$(() => {
+// $(() => {
 
-    const text = $("#text");
-    text.addClass("float-right");
+//     const text = $("#text");
+//     text.addClass("float-right");
 
-});
+// });
+
+const text = document.querySelector("#text");
+
+text.classList.add('float-end'); // float-end = float-right (ancienne version bootstrap);
